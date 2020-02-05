2779e0192a7cbf31ec803f6816ed98aaf21b706f
Michał Poręba <michalporeba@gmail.com>
2020-02-05 18:19:31 +0000

    renaming assert methods

--- JPTests.java
+++ JPTests.java
@@ -1,102 +1,59 @@
-/**
- *  A single base class for writing simple class unit tests
- *  for mor information https://github.com/michalporeba/jptests
- *  @version 0.1.0 
- */
-
 import java.lang.Class;
 import java.lang.Exception;
 
-public class JPTests
+/**
+ * A single base class for writing simple class unit tests.
+ * For mor information see <a href="https://github.com/michalporeba/jptests">JPTest Repository</a>.
+ *  
+ * @version 0.1.0 
+ */
+
+public abstract class JPTests
 {
 	protected static int assertions = 0;
 	protected static int failed = 0;
 	protected static int passed = 0;
 
-	protected static void AssertAreEqual(char expected, char actual, String description)
+	protected static void assertAreEqual(char expected, char actual, String description)
 	{
-		if (expected != actual)
-		{
-			System.out.printf("FAIL: %s should be %c but was %c%n", description, expected, actual);
-			++failed;
-		}
-		else 
-		{
-			System.out.printf("PASS: %s%n", description);
-			++passed;
-		}
+		assertAreEqual(new Character(expected), new Character(actual), "c", description);
 	}
 
-	protected static void AssertAreEqual(int expected, int actual, String description)
+	protected static void assertAreEqual(int expected, int actual, String description)
 	{
-		if (expected != actual)
-		{
-			System.out.printf("FAIL: %s should be %d but was %d%n", description, expected, actual);
-			++failed;
-		}
-		else 
-		{
-			System.out.printf("PASS: %s%n", description);
-			++passed;
-		}
+		assertAreEqual(new Integer(expected), new Integer(actual), "d", description);
 	}
 
-	protected static void AssertAreEqual(double expected, double actual, String description)
+	protected static void assertAreEqual(double expected, double actual, String description)
 	{
-		if (expected != actual) 
+		assertAreEqual(new Double(expected), new Double(actual), "f", description);
+	}
+	
+	protected static void assertAreEqual(boolean expected, boolean actual, String description)
+	{
+		assertAreEqual(new Boolean(expected), new Boolean(actual), "s", description);
+	}
+
+	protected static void assertAreEqual(String expected, String actual, String description)
+	{
+		assertAreEqual(expected, actual, "s", description);
+	}
+
+	protected static void assertIsNotNull(Object obj, String description) 
+	{
+		if (obj != null)
 		{
-			System.out.printf("FAIL: %s should be %f but was %f%s", description, expected, actual);
-			++failed;
+			System.out.printf("[PASS] %s%n", description);
+			++passed;
 		}
 		else 
 		{
-			System.out.printf("PASS: %s%n", description);
+			System.out.printf("[FAIL] %s%n\tExpected: NULL%n\tActual: %s%n", description, obj);
+			++failed;
 		}
 	}
 	
-	protected static void AssertAreEqual(boolean expected, boolean actual, String description)
-	{
-		if (expected != actual)
-		{
-			System.out.printf("FAIL: %s should be %s but was %s%n", description, expected, actual);
-			++failed;
-		}
-		else 
-		{
-			System.out.printf("PASS: %s%n", description);
-			++passed;
-		}
-	}
-
-	protected static void AssertAreEqual(String expected, String actual, String description)
-	{
-		if (!expected.equals(actual))
-		{
-			System.out.printf("FAIL: %s should be %s but was %s%n", description, expected, actual);
-			++failed;
-		}
-		else 
-		{
-			System.out.printf("PASS: %s%n", description);
-			++passed;
-		}
-	}
-
-	protected static void AssertIsNotNull(Object obj, String description) 
-	{
-		if (obj == null)
-		{
-			System.out.printf("FAIL: %s", description);
-			++failed;
-		}
-		else 
-		{
-			System.out.printf("PASS: %s", description);
-			++passed;
-		}
-	}
-	
-	protected static void AssertThrows(SimpleAction action, Class<? extends Exception> exceptionType, String description)
+	protected static void assertThrows(SimpleAction action, Class<? extends Exception> exceptionType, String description)
 	{	
 		try 
 		{
@@ -120,9 +77,23 @@ public class JPTests
 		
 	}
 
+	private static <T> void assertAreEqual(T expected, T actual, String format, String description)
+	{
+		if ((expected == null && actual == null)
+			|| expected.equals(actual))
+		{
+			System.out.printf("[PASS] %s%n", description);
+			++passed;
+		}
+		else 
+		{
+			System.out.printf("[FAIL] %s%n\tExpected: %" + format + "%n\tActual:  %" + format + "%n", description, expected, actual);
+			++failed;
+		}
+	}
+
 	public interface SimpleAction
 	{
 		void execute();
 	}
 }
-
