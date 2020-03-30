d26c8668bc58f1b89db5de70e68a1a1fbf4c7ace
Michał Poręba <michalporeba@gmail.com>
2020-03-30 23:59:06 +0100

    listing only public, void and parameterless methods

--- experiments/SampleShould.java
+++ experiments/SampleShould.java
@@ -2,12 +2,12 @@ public class SampleShould
 {
     public void be_a_test()
     {
-
+        System.out.println("A");
     }
 
     public void be_another_test()
     {
-
+        System.out.println("B");
     }
 
     public int not_be_a_test_as_it_has_non_void_return_type()
@@ -16,6 +16,11 @@ public class SampleShould
     }
 
     public void not_be_a_test_as_it_has_parameters(int a)
+    {
+
+    }
+
+    private void this_is_private()
     {
         
     }
--- experiments/Test.java
+++ experiments/Test.java
@@ -1,13 +1,36 @@
 import java.lang.reflect.Method;
-
+import java.lang.reflect.Modifier;
+import java.lang.reflect.InvocationTargetException;
 public class Test
 {
     public static void main(String[] args)
     {
         for(Method m : SampleShould.class.getDeclaredMethods())
         {
-            System.out.print(m.getDeclaringClass().getName() + ".");
-            System.out.println(m.getName());
+            if (
+                Modifier.isPublic(m.getModifiers())
+                && !Modifier.isAbstract(m.getModifiers())
+                && !Modifier.isStatic(m.getModifiers())
+                && m.getReturnType().equals(Void.TYPE)
+                && m.getParameterCount() == 0
+            )
+            {
+                System.out.print(m.getDeclaringClass().getName() + ".");
+                System.out.println(m.getName());
+                try 
+                {
+                    SampleShould c = new SampleShould();
+                    m.invoke(c, (Object[])null);
+                // } catch (ClassNotFoundException x) {
+                //     x.printStackTrace();
+                // // } catch (NoSuchMethodException x) {
+                // //     x.printStackTrace();
+                } catch (IllegalAccessException x) {
+                    x.printStackTrace();
+                } catch (InvocationTargetException x) {
+                    x.printStackTrace();
+                }
+            }
         }
     }
 }
